69b8f9808e57167f3190c973fd38334a01860dac
yingyanwu <1179858356@qq.com>
2020-08-25 08:22:00 +0800

    屏幕适配测试

--- app/src/main/java/com/wyy/bizwithjet/MainActivity.kt
+++ app/src/main/java/com/wyy/bizwithjet/MainActivity.kt
@@ -15,20 +15,30 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        DisplayUtil.setCustomDensityByHeight(this, application, 400)
+//        DisplayUtil.setCustomDensityByHeight(this, application, 785)
+        DisplayUtil.setCustomDensityByHeight(this, application, 864)
         setDhlAndZtl()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        mWindowWidthDp = (1280.0 / resources.displayMetrics.density + 0.5).toInt()
-        mWindowHeightDp = (800.0 / resources.displayMetrics.density + 0.5).toInt()
+//        mWindowWidthDp = (1280.0 / resources.displayMetrics.density + 0.5).toInt()
+//        mWindowHeightDp = (800.0 / resources.displayMetrics.density + 0.5).toInt()
+        mWindowWidthDp = (1080.0 / resources.displayMetrics.density + 0.5).toInt()
+        mWindowHeightDp = (2160.0 / resources.displayMetrics.density + 0.5).toInt()
         mWidthPx = windowManager.defaultDisplay.width
         mHeightPx = windowManager.defaultDisplay.height
         binding.tvContent.text = "屏幕宽度是:$mWindowWidthDp dp，高度是：$mWindowHeightDp dp。$mWidthPx px, $mHeightPx px"
     }
 
     private fun setDhlAndZtl() {
-        window.decorView.apply {
-            systemUiVisibility =
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-        }
+//        window.decorView.apply {
+//            systemUiVisibility =
+//                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+//        }
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
     }
 }
